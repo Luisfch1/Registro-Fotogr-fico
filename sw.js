4f8642a6e8a342f6d0259045baa4f7113e4c212d
Luisfch1 <ingenierocuellar1@gmail.com>
2025-12-14 19:20:00 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE_VERSION = "v11.1";
+const CACHE_VERSION = "v11";
 const CACHE = `rf-cache-${CACHE_VERSION}`;
 
 const ASSETS = [
@@ -78,6 +78,5 @@ self.addEventListener("fetch", (event) => {
     }
   })());
 });
-
 
 
